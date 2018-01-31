f2e76078dc2eb5855b4c702276c07a2d75407e5f
Jace Riehl <riehlj4@36flash.cs.uleth.ca>
2018-01-31 15:59:18 -0700

    Added some documentation

--- GoFish/include/Card.h
+++ GoFish/include/Card.h
@@ -7,17 +7,45 @@ using namespace std;
 class Card
 {
     public:
+        /**
+        *Constructor to build a card.
+        *@param string face of card
+        *@param string suit of card
+        */
         Card(string f, string s);
+        /**
+        *Getter for the suit of the card.
+        *@return string suit
+        */
         string getSuit();
+        /**
+        *Getter for the face of the card.
+        *@return string face
+        */
         string getFace();
+        /**
+        *Destructor
+        */
         virtual ~Card();
 
     protected:
 
     private:
+        /**
+        *Suit of current Card object.
+        */
         string suit;
+        /**
+        *Face of the current Card object.
+        */
         string face;
+        /**
+        *The faces that the card can be.
+        */
         string faces[13] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King","Ace"};
+        /**
+        *The suits that the card can be.
+        */
         string suits[4] = {"Spades", "Clubs", "Hearts", "Diamonds"};
 };
 
--- GoFish/include/Deck.h
+++ GoFish/include/Deck.h
@@ -10,15 +10,34 @@ using namespace std;
 class Deck
 {
     public:
+        /**
+        *Constructor to build a deck of a standard 52 card deck.
+        */
         Deck();
+        /**
+        *Destructor for Deck.
+        */
         virtual ~Deck();
+        /**
+        Shuffles the current deck, no matter what the size is
+        */
         void shuffleDeck();
+        /**
+        *Constructor to build a card.
+        *@param The Card that is dealt that is removed from the deck
+        */
         Card dealCard();
+        /**
+        *Prints the deck to console - to be deleted.
+        */
         void printDeck();
 
     protected:
 
     private:
+        /**
+        *Current deck
+        */
         int maxCards = 52;
         vector<Card> deck;
         int indexCurrent = 0;
