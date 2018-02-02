2c984dc26c28d013b3716dbcba8f908f3fbe27a4
riehlj4 <jace.riehl@uleth.ca>
2018-02-01 18:04:13 -0700

    Added functions to player

--- GoFish/include/Player.h
+++ GoFish/include/Player.h
@@ -2,16 +2,23 @@
 #define PLAYER_H
 #include <vector>
 #include "Card.h"
+#include <algorithm>
 using namespace std;
 class Player
 {
     public:
         Player(int pN);
         ~Player();
+        void addToHand(vector<Card> c);
+        vector<Card> takeFromHand(Card);
+        bool checkMatchingPairs();
+
+
 
     protected:
 
     private:
+        void sortHand();
         int playerNumber;
         int matchingPairs = 0;
         vector<Card> currentHand;
--- GoFish/src/Player.cpp
+++ GoFish/src/Player.cpp
@@ -5,3 +5,31 @@ Player::Player(int pN)
     playerNumber = pN;
 }
 
+void Player::addToHand(vector<Card> c)
+{
+    //currentHand += c;
+    sortHand();
+}
+
+vector<Card> Player::takeFromHand(Card c)
+{
+    vector<Card> ret;
+    for(int i=0;i<currentHand.size();i++)
+    {
+        if(c == currentHand[i])
+            ret.push_back(currentHand[i]);
+    }
+    return ret;
+}
+
+bool Player::checkMatchingPairs()
+{
+    return true;
+}
+
+void Player::sortHand()
+{
+    //sort(currentHand.begin(), currentHand.end());
+}
+
+
