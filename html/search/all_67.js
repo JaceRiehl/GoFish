7fa59012c1c5b842fdc12ffed5430d32918a5e9b
riehlj4 <jace.riehl@uleth.ca>
2018-02-03 18:44:49 -0700

    Attempting to fix pipeline

--- html/search/all_67.js
+++ html/search/all_67.js
@@ -4,6 +4,8 @@ var searchData=
   ['getdecksize',['getDeckSize',['../class_deck.html#ad775d4b99ec86991402ad12266b1b157',1,'Deck']]],
   ['getface',['getFace',['../class_card.html#afb2b73b451cc753663edab09abfc4bd6',1,'Card']]],
   ['gethand',['getHand',['../class_player.html#a6ca673336972f29fdc094b3f0f1f7f84',1,'Player']]],
+  ['getmatchedhand',['getMatchedHand',['../class_player.html#a09fbf15e6a001feea60cb53d02ec3912',1,'Player']]],
+  ['getnummatches',['getNumMatches',['../class_player.html#abdea2b45e1a98e9942bdaca85f451734',1,'Player']]],
   ['getplayernumber',['getPlayerNumber',['../class_player.html#ae72235dee0a2f7abbcb82002164623aa',1,'Player']]],
   ['getsuit',['getSuit',['../class_card.html#a6bda7d706eb87b009c94ae2a9654f6d9',1,'Card']]]
 ];
