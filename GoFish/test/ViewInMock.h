7cca6984baceeac0ff3fc6225c4ab071c145a75a
riehlj4 <jace.riehl@uleth.ca>
2018-02-08 15:35:30 -0700

    Added mocks for View

---
+++ GoFish/test/ViewInMock.h
@@ -0,0 +1,18 @@
+#ifndef VIEWINMOCK_H
+#define VIEWINMOCK_H
+#inlcude "ViewIn.h"
+#include "gmock/gmock.h"
+
+class ViewInMock : public ViewIn
+{
+    public:
+        MOCK_METHOD2(choosePlayer,int(int numPlayers,int playerNum));
+        MOCK_METHOD0(chooseNumPlayers,int());
+        MOCK_METHOD1(chooseCard,int(int maxCard));
+
+    protected:
+
+    private:
+};
+
+#endif // VIEWINMOCK_H
